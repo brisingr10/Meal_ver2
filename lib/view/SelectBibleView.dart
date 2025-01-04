f7dfd6a009b5edea13d081f8ae8f74d91fe36419
LeeJeongWoog <leejw0523@gmail.com>
2025-01-04 22:20:49 +0900

    성경 선택 Ui 변경 중

--- lib/view/SelectBibleView.dart
+++ lib/view/SelectBibleView.dart
@@ -26,22 +26,74 @@ class _SelectBibleViewState extends State<SelectBibleView> {
     // ViewModel에서 저장된 선택 상태를 가져와 Map에 반영
     final viewModel = Provider.of<MainViewModel>(context, listen: false);
 
-    bool isAnySelected = false;
-
+    // 초기화: ViewModel에서 가져온 데이터를 반영
     for (String bible in viewModel.SelectedBibles) {
       if (selectedBibles.containsKey(bible) && !selectedOrder.contains(bible)) {
         selectedBibles[bible] = true;
-        selectedCount++;
         selectedOrder.add(bible);
-        isAnySelected = true;
       }
     }
-    // 아무것도 선택되지 않았을 경우, '개역개정'을 기본 선택 상태로 설정
-    if (!isAnySelected && selectedBibles.containsKey('개역개정')  && !selectedOrder.contains('개역개정')) {
-      selectedBibles['개역개정'] = true;
-      selectedCount++;
-      selectedOrder.add('개역개정');
-    }
+
+    // 화면 내 모든 성경을 selectedBibles에 포함
+    selectedBibles.forEach((key, value) {
+      if (value == true && !selectedOrder.contains(key)) {
+        selectedOrder.add(key);
+      }
+    });
+
+  }
+
+  void _addBible() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        List<String> availableBibles = selectedBibles.entries
+            .where((entry) => entry.value == false) // value가 true인 항목만 필터링
+            .map((entry) => entry.key)            // key만 추출
+            .toList();
+        return AlertDialog(
+          title: Text("추가할 성경 선택"),
+          content:SizedBox(
+        width: 400,  // 너비 제한 추가
+          child : ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: 300, // 최대 높이 제한
+              minWidth: 300,  // 최소 너비 제한
+              maxWidth: 400,  // 최대 너비 제한 추가
+            ),
+
+          child: availableBibles.isNotEmpty
+              ? ListView.builder(
+            shrinkWrap: true,
+            itemCount: availableBibles.length,
+            itemBuilder: (context, index) {
+              String bible = availableBibles[index];
+              return ListTile(
+                title: Text(bible),
+                onTap: () {
+                  setState(() {
+                    selectedBibles[bible] = true;
+                    selectedOrder.add(bible);
+                  });
+                  Navigator.pop(context);
+                },
+              );
+            },
+          )
+              : Text("추가 가능한 성경이 없습니다."),
+        ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("닫기"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -64,6 +116,12 @@ class _SelectBibleViewState extends State<SelectBibleView> {
                 color: themeMode == ThemeMode.dark ? Colors.white : Colors.black,
                   ),
             ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: _addBible,
+              ),
+            ],
           ),
               body: Column(
                 children: [
@@ -71,27 +129,43 @@ class _SelectBibleViewState extends State<SelectBibleView> {
                     child: ReorderableListView(
                       onReorder: (int oldIndex, int newIndex) {
                         setState(() {
-                          // 드래그 시 항목을 이동
-                          if (newIndex > oldIndex) newIndex--; // 리스트 이동에 맞게 조정
+                          if (newIndex > oldIndex) newIndex--;
                           final String movedBible = selectedOrder.removeAt(oldIndex);
                           selectedOrder.insert(newIndex, movedBible);
-
-                          // `selectedBibles` 상태도 업데이트
-                          final selectedBiblesKeys = selectedBibles.keys.toList();
-                          final movedKey = selectedBiblesKeys.removeAt(oldIndex);
-                          selectedBiblesKeys.insert(newIndex, movedKey);
-
-                          final newSelectedBibles = <String, bool>{};
-                          for (final key in selectedBiblesKeys) {
-                            newSelectedBibles[key] = selectedBibles[key]!;
-                          }
-                          selectedBibles = newSelectedBibles;
                         });
                       },
-                      children: selectedBibles.keys.map((String bible) {
-                        int orderIndex = selectedOrder.indexOf(bible) + 1;
-                        return ListTile(
-                          key: ValueKey(bible), // 고유 키 설정
+              children: selectedOrder.map((String bible) {
+                return Dismissible(
+                  key: ValueKey(bible),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    setState(() {
+                      selectedOrder.remove(bible);
+                      selectedBibles.remove(bible);
+                    });
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 20),
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      bible,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    trailing: Icon(Icons.list, color: Theme.of(context).colorScheme.primary), // 항상 List 아이콘 표시
+                    // `onTap` 제거
+                  ),
+                );
+              }).toList(),
+            ),
+                  ),
+                      // children: selectedBibles.keys.map((String bible) {
+                      //   int orderIndex = selectedOrder.indexOf(bible) + 1;
+                      //   return ListTile(
+                      //     key: ValueKey(bible), // 고유 키 설정
                           // leading: CircleAvatar(
                           //   backgroundColor: (selectedBibles[bible] ?? false)
                           //       ? Theme.of(context).colorScheme.primary
@@ -103,33 +177,33 @@ class _SelectBibleViewState extends State<SelectBibleView> {
                           //     ),
                           //   ),
                           // ),
-                          title: Text(
-                            bible,
-                            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                              color: themeMode == ThemeMode.dark ? Colors.white : Colors.black,
-                            ),
-                          ),
-                          trailing: selectedBibles[bible] == true
-                              ? Icon(
-                            Icons.check,
-                            color: Theme.of(context).colorScheme.primary,
-                          )
-                              : null, // 선택된 경우 체크 아이콘 표시
-                          onTap: () {
-                            setState(() {
-                              if (selectedBibles[bible] ?? false) {
-                                selectedBibles[bible] = false;
-                                selectedOrder.remove(bible);
-                              } else {
-                                selectedBibles[bible] = true;
-                                selectedOrder.add(bible);
-                              }
-                            });
-                          },
-                        );
-                      }).toList(),
-                    ),
-                  ),
+                    //       title: Text(
+                    //         bible,
+                    //         style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    //           color: themeMode == ThemeMode.dark ? Colors.white : Colors.black,
+                    //         ),
+                    //       ),
+                    //       trailing: selectedBibles[bible] == true
+                    //           ? Icon(
+                    //         Icons.check,
+                    //         color: Theme.of(context).colorScheme.primary,
+                    //       )
+                    //           : null, // 선택된 경우 체크 아이콘 표시
+                    //       onTap: () {
+                    //         setState(() {
+                    //           if (selectedBibles[bible] ?? false) {
+                    //             selectedBibles[bible] = false;
+                    //             selectedOrder.remove(bible);
+                    //           } else {
+                    //             selectedBibles[bible] = true;
+                    //             selectedOrder.add(bible);
+                    //           }
+                    //         });
+                    //       },
+                    //     );
+                    //   }).toList(),
+                    // ),
+                  //),
                   // Expanded(
                   //   child: ListView(
                   //     children: selectedBibles.keys.map((String bible) {
@@ -174,10 +248,8 @@ class _SelectBibleViewState extends State<SelectBibleView> {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: ElevatedButton(
-                  onPressed: selectedCount > 0 ? () {
-                    // 선택된 파일들만 리스트로 필터링
+                  onPressed: selectedOrder.isNotEmpty ? () {
                     viewModel.loadMultipleBibles(selectedOrder);
-                    // 창 종료
                     Navigator.pop(context);
                   } : null,
                   child: Text(
