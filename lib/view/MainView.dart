eab4c6e797f434a6922774841ff9658b13706ebc
LeeJeongWoog <leejw0523@gmail.com>
2025-01-11 18:33:39 +0900

    btext 간격 조절 완료

--- lib/view/MainView.dart
+++ lib/view/MainView.dart
@@ -183,6 +183,7 @@ class _Meal2ViewState extends State<Meal2View> {
                                                         fontWeight: FontWeight.normal,
                                                         fontFamily: 'Biblefont',
                                                         fontSize: viewModel.fontSize,
+                                                        height: 1.7,
                                                       ),
                                                     ),
                                                   ),
